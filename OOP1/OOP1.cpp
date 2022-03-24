3e469e8c69d8ff475f708cd8e8123d9a62c0963b
User-ai-cell <67262438+User-ai-cell@users.noreply.github.com>
2022-03-24 15:34:35 +0300

    Add files via upload

---
+++ OOP1/OOP1.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+using namespace std;
+
+// Условия существования прямоугольного треугольника;
+// 1. Все стороны больше нуля;
+// 2. Большая сторона равна сумме квадратов двух остальных;
+
+// класс прям треуг;
+class triangle
+{
+private:
+	int first_side; // 1 сторона;
+	int sec_side; // 2 сторона;
+	int third_side; // 3 сторона;
+public:
+	void max(int a, int b, int c) // функция подстановки квадрата максимума и двух остальных значений, а также опредиляет прямоугольный ли треугольник;
+	{
+		int max, zn1, zn2;
+		first_side = a;
+		sec_side = b;
+		third_side = c;
+
+		if (a == b || a == c)
+		{
+			max = a;
+			zn1 = b;
+			zn2 = c;
+		}
+		if (b == a || b == c)
+		{
+			max = b;
+			zn1 = a;
+			zn2 = c;
+		}
+		if (c == b || c == a)
+		{
+			max = c;
+			zn1 = b;
+			zn2 = a;
+		}
+		if (a > b && a > c)
+		{
+			max = a;
+			zn1 = b;
+			zn2 = c;
+		}
+		if (b > a && b > c)
+		{
+			max = b;
+			zn1 = a;
+			zn2 = c;
+		}
+		if (c > b && c > a)
+		{
+			max = c;
+			zn1 = b;
+			zn2 = a;
+		}
+		int tmp_max = max * max;
+		int tmp_zn1 = zn1 * zn1;
+		int tmp_zn2 = zn2 * zn2;
+		if (tmp_max == tmp_zn1 + tmp_zn2 && !isdigit(tmp_max) && !isdigit(tmp_zn2) && !isdigit(tmp_zn1))
+			cout << "\nДанная фигура является прямоугольным треугольником\n";
+		else if (a <= 0 || b <= 0 || c <= 0)
+			cout << "\nДанная фигура не является треугольником\n";
+		else
+			cout << "\nДанная фигура не является прямоугольным треугольником\n";
+
+	}
+	// меню;
+	void menu()
+	{
+		cout << "\n";
+		cout << "Программа определения прямоугольного треугольника по значениям 3-х сторон\n\n";
+		cout << "1. Введите стороны треугольника\n\n";
+		cout << "(q для завершения) ";
+	}
+	// управление меню;
+	bool isvalid(char ch)
+	{
+		if (ch != '1' && ch != 'q')
+			return false;
+		else
+			return true;
+	}
+
+};
+// функция проверки ввода данных;
+int test(int a, int b, int c)
+{
+	while (!cin.good() || isdigit(a))
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\nВведен неверный символ!" << endl;
+		break;
+		return 0;
+	}
+	while (!cin.good() || isdigit(b))
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\nВведен неверный символ!" << endl;
+		break;
+		return 0;
+	}
+	while (!cin.good() || isdigit(c))
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "\nВведен неверный символ!" << endl;
+		break;
+		return 0;
+	}
+}
+
+int main()
+{
+	int a, b, c;
+	char choice;
+	triangle Obj;
+
+	for (;;) {
+		do {
+			Obj.menu();
+			cin >> choice;
+		} while (!Obj.isvalid(choice));
+		if (choice == 'q') break;
+		cout << "\n";
+		cin >> a >> b >> c;
+		test(a, b, c);
+		Obj.max(a, b, c);
+	}
+	return 0;
+}
---
+++ OOP2/OOP2.cpp
@@ -0,0 +1,105 @@
+﻿// OOP2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Worker
+{
+private:
+    string name;
+    string surname;
+    int rate;
+    int days;
+    int copynumber;
+public:
+
+    int getSalary() 
+    { 
+        return rate * days;
+    }
+
+    // конструктор по умолчанию
+    Worker()
+    {
+        rate = 1500;
+        days = 1;
+        name = "Иван";
+        surname = "Иванов";
+        copynumber = 0;
+        cout << "\nИмя, фамилия сотрудника: " << name + surname << endl;
+        cout << "\nСтавка: " << rate << " рублей \n";
+        cout << "\nКол-во смен: " << days <<"\n";
+    }
+    // конструктор для установки начальных значений полей
+    Worker(string n, string s, int r, int d)
+    {
+        copynumber = 0;
+        cout << "\nВведите имя сотрудника: ";
+        getline(cin, n);
+        name = n;
+        cout << "Введите фамилию сотрудника: ";
+        getline(cin, s);
+        surname = s;
+        cout << "Введите cтавку: ";
+        cin >> r;
+        rate = r;
+        cout << "Введите кол-во смен: ";
+        cin >> d;
+        days = d;
+    }
+    // конструктор копирования
+    Worker(const Worker &obj)
+    {
+        rate = obj.rate;
+        days = obj.days;
+        name = obj.name;
+        surname = obj.surname;
+        copynumber = obj.copynumber + 1;
+        cout << "\nСоздание копии...\n\n";
+    }
+
+    ~Worker()
+    {
+        if (copynumber == 0)
+            cout << "\nУничтожение оригинала.\n\n";
+        else
+            cout << "Уничтожение копии " << copynumber << "\n";
+    }
+
+};
+
+
+void display(Worker ob)
+{
+    cout << "Зарплата сотрудника: " << ob.getSalary() << " Рублей.\n" << endl;
+}
+
+int main()
+{
+    string n;
+    string s;
+
+    int r, d;
+    r = 0;
+    d = 0;
+
+    Worker ob1;
+
+    Worker ob2(n, s, r, d);
+
+    Worker ob3 = ob1;
+
+    //Конструктор копирования вызывается, когда объект передается (функции) по значению в качестве аргумента
+
+    display(ob1);
+
+    display(ob2);
+
+    display(ob3);
+
+    return 0;
+}
+
